bfcf8581fb81057bd6f1bf8a0edb5083c4849b02
Tin Nguyen <tindn14@gmail.com>
2018-11-30 10:51:21 -0500

    adding custom theme config

--- App.js
+++ App.js
@@ -1,49 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import { Button, ThemeProvider } from 'react-native-elements';
+import { Platform, View } from 'react-native';
+import { Button, ThemeProvider, withTheme } from 'react-native-elements';
+import customTheme from './customTheme.json';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-});
-
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
-      <ThemeProvider>
-        <View style={styles.container}>
-          <Button title="hey" />
-        </View>
-      </ThemeProvider>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Button title="hey" />
+      </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
-});
+export default () => (
+  <ThemeProvider theme={customTheme}>
+    <App />
+  </ThemeProvider>
+);
