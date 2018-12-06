8c1d9714426b8948b0066b562de2ea0e21e3ed02
Tin Nguyen <tindn14@gmail.com>
2018-12-06 10:17:38 -0500

    refactor transaction list logic

--- app/money/expenses.js
+++ app/money/expenses.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import { RefreshControl, ScrollView } from 'react-native';
+import {
+  RefreshControl,
+  ScrollView,
+  TouchableOpacity,
+  Text
+} from 'react-native';
 import Screen from '../screen';
 import SpendTracking from './spendTracking';
 import TransactionForm from './transactionForm';
 import TransactionList from './transactionList';
+import { getTransactionsCollection, fetchTransactions } from '../../utils';
+import theme from '../../theme';
 
 class Expenses extends React.Component {
   static navigationOptions = {
     header: null
   };
   state = {
-    refreshing: false
+    refreshing: false,
+    tenDaysTransactions: [],
+    fetchingTenDaysTransactions: false
   };
 
-  onRefresh = () => {
-    this.setState(
-      () => ({ refreshing: true }),
-      () => {
-        console.log('refreshing');
-        setTimeout(() => this.setState({ refreshing: false }), 2000);
-      }
+  componentDidMount() {
+    getTransactionsCollection().onSnapshot(this.getTenDaysTransactions);
+    this.fetchData();
+  }
+
+  getTenDaysTransactions = () => {
+    this.setState({ fetchingTenDaysTransactions: true });
+    let tenDaysAgo = new Date(Date.now() - 864000000);
+    tenDaysAgo = new Date(
+      tenDaysAgo.getFullYear(),
+      tenDaysAgo.getMonth(),
+      tenDaysAgo.getDate()
     );
+    return fetchTransactions([
+      ['where', 'date', '>=', tenDaysAgo],
+      ['orderBy', 'date', 'desc']
+    ]);
+  };
+
+  fetchData = () => {
+    this.setState({ refreshing: true });
+    this.getTenDaysTransactions().then(transactions => {
+      this.setState({
+        tenDaysTransactions: transactions,
+        fetchingTenDaysTransactions: false,
+        refreshing: false
+      });
+    });
   };
 
   render() {
@@ -32,13 +61,31 @@ class Expenses extends React.Component {
           refreshControl={
             <RefreshControl
               refreshing={this.state.refreshing}
-              onRefresh={this.onRefresh}
+              onRefresh={this.fetchData}
             />
           }
         >
           <TransactionForm />
           <SpendTracking />
-          <TransactionList />
+          <TouchableOpacity
+            onPress={() => {}}
+            style={{
+              padding: 7,
+              backgroundColor: theme.colors.lighterGray,
+              borderRadius: 5,
+              width: 100,
+              alignSelf: 'center',
+              top: 7,
+              zIndex: 1,
+              marginTop: 20
+            }}
+          >
+            <Text style={{ textAlign: 'center' }}>past 10 days</Text>
+          </TouchableOpacity>
+          <TransactionList
+            data={this.state.tenDaysTransactions}
+            refreshing={this.state.fetchingTenDaysTransactions}
+          />
         </ScrollView>
       </Screen>
     );
--- app/money/transactionList.js
+++ app/money/transactionList.js
@@ -1,121 +1,54 @@
 import React from 'react';
-import {
-  FlatList,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View
-} from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import theme from '../../theme';
-import {
-  fetchTransactions,
-  getTransactionsCollection,
-  toDateString
-} from '../../utils';
+import { toDateString } from '../../utils';
 
-class TransactionList extends React.Component {
-  state = {
-    transactions: [],
-    fetchingTransactionList: false
-  };
-
-  componentDidMount() {
-    getTransactionsCollection().onSnapshot(this.getTransactions);
-    this.getTransactions();
-  }
-
-  getTransactions = () => {
-    let tenDaysAgo = new Date(Date.now() - 864000000);
-    tenDaysAgo = new Date(
-      tenDaysAgo.getFullYear(),
-      tenDaysAgo.getMonth(),
-      tenDaysAgo.getDate()
-    );
-
-    this.setState(
-      function() {
-        return {
-          fetchingTransactionList: true
-        };
-      },
-      function() {
-        fetchTransactions([
-          ['where', 'date', '>=', tenDaysAgo],
-          ['orderBy', 'date', 'desc']
-        ])
-          .then(transactions => {
-            this.setState({ transactions, fetchingTransactionList: false });
-          })
-          .catch(e => this.setState({ fetchingTransactionList: false }));
-      }.bind(this)
-    );
-  };
-
-  render() {
-    return (
-      <View style={{ marginTop: 20 }}>
-        <TouchableOpacity
-          onPress={this.getTransactions}
+function TransactionList(props) {
+  const { data, refreshing } = props;
+  return (
+    <FlatList
+      refreshing={refreshing}
+      onRefresh={() => {}}
+      data={data}
+      keyExtractor={item => item.id}
+      ListEmptyComponent={
+        <View
           style={{
-            padding: 7,
-            backgroundColor: theme.colors.lighterGray,
-            borderRadius: 5,
-            width: 100,
-            alignSelf: 'center',
-            top: 7,
-            zIndex: 1
+            backgroundColor: '#fff',
+            padding: 20,
+            alignItems: 'center'
           }}
         >
-          <Text style={{ textAlign: 'center' }}>past 10 days</Text>
-        </TouchableOpacity>
-        <FlatList
-          refreshing={this.state.fetchingTransactionList}
-          onRefresh={() => {}}
-          data={this.state.transactions}
-          keyExtractor={item => item.id}
-          ListEmptyComponent={
-            <View
-              style={{
-                backgroundColor: '#fff',
-                padding: 20,
-                alignItems: 'center'
-              }}
-            >
-              <Text>No transactions found.</Text>
+          <Text>No transactions found.</Text>
+        </View>
+      }
+      renderItem={({ item, index }) => {
+        const borderBottomWidth = index === data.length - 1 ? 0 : 1;
+        let dateDisplay = item.date;
+        const color =
+          item.entryType === 'debit' ? theme.colors.red : theme.colors.green;
+        if (item.date.toDate) {
+          dateDisplay = toDateString(item.date);
+        }
+        return (
+          <View
+            style={[
+              styles.transactionItem,
+              { borderBottomWidth: borderBottomWidth }
+            ]}
+          >
+            <View>
+              <Text style={styles.transactionItemMemo}>{item.memo}</Text>
+              <Text style={styles.transactionItemDate}>{dateDisplay}</Text>
             </View>
-          }
-          renderItem={({ item, index }) => {
-            const borderBottomWidth =
-              index === this.state.transactions.length - 1 ? 0 : 1;
-            let dateDisplay = item.date;
-            const color =
-              item.entryType === 'debit'
-                ? theme.colors.red
-                : theme.colors.green;
-            if (item.date.toDate) {
-              dateDisplay = toDateString(item.date);
-            }
-            return (
-              <View
-                style={[
-                  styles.transactionItem,
-                  { borderBottomWidth: borderBottomWidth }
-                ]}
-              >
-                <View>
-                  <Text style={styles.transactionItemMemo}>{item.memo}</Text>
-                  <Text style={styles.transactionItemDate}>{dateDisplay}</Text>
-                </View>
-                <Text style={[styles.transactionItemAmount, { color }]}>
-                  $ {item.amount.toFixed(2)}
-                </Text>
-              </View>
-            );
-          }}
-        />
-      </View>
-    );
-  }
+            <Text style={[styles.transactionItemAmount, { color }]}>
+              $ {item.amount.toFixed(2)}
+            </Text>
+          </View>
+        );
+      }}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
@@ -144,4 +77,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default TransactionList;
+export default React.memo(TransactionList);
