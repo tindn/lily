6cee5af325959ce95b4e1909a49690cf73f85dda
Tin Nguyen <tindn14@gmail.com>
2018-12-06 12:07:12 -0500

    expandable input form

--- app/money/transactionForm.js
+++ app/money/transactionForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   DatePickerIOS,
+  LayoutAnimation,
   Modal,
   StyleSheet,
   Switch,
@@ -17,15 +18,20 @@ const amountRegex = /^\$\s[0]*([1-9]*)\.*(\d*)$/;
 const smallFontSize = 20;
 const largeFontSize = 26;
 const inputBorderColor = theme.colors.lighterGray;
-const defaultState = {
-  date: new Date(),
-  memo: '',
-  amount: '$ 00.00',
-  isCredit: false,
-  dateModalVisible: false
-};
+
+function getDefaultState() {
+  return {
+    date: new Date(),
+    memo: '',
+    amount: '$ 00.00',
+    isCredit: false,
+    dateModalVisible: false,
+    isExpanded: false
+  };
+}
+
 class TransactionForm extends React.Component {
-  state = { ...defaultState };
+  state = getDefaultState();
 
   onChangeText = text => {
     // need to determine recent changes (.ie backspace vs. number)
@@ -52,42 +58,20 @@ class TransactionForm extends React.Component {
 
   render() {
     return (
-      <View
-        style={{
-          shadowColor: '#000',
-          shadowOffset: { width: 0, height: 2 },
-          shadowOpacity: 0.23,
-          shadowRadius: 2.62,
-          elevation: 4,
-          marginLeft: 5,
-          marginRight: 5,
-          marginTop: 5,
-          backgroundColor: theme.colors.backgroundColor,
-          borderRadius: 10
-        }}
-      >
-        <View
-          style={{
-            flexDirection: 'row',
-            borderBottomColor: inputBorderColor,
-            borderBottomWidth: 1,
-            padding: 12,
-            backgroundColor: '#fff',
-            borderTopLeftRadius: 10,
-            borderTopRightRadius: 10
+      <View style={sharedStyles.container}>
+        <TouchableOpacity
+          onPress={() => {
+            LayoutAnimation.easeInEaseOut();
+            this.setState({ isExpanded: true });
           }}
+          style={sharedStyles.firstRow}
         >
           <View>
             <TouchableOpacity
               onPress={() => this.setState({ dateModalVisible: true })}
             >
               <Text
-                style={{
-                  flex: 9,
-                  fontSize: smallFontSize,
-                  color: theme.colors.darkGray,
-                  fontWeight: '500'
-                }}
+                style={sharedStyles.dateText}
                 onChangeText={text => this.setState({ date: text })}
                 keyboardType="decimal-pad"
               >
@@ -99,18 +83,7 @@ class TransactionForm extends React.Component {
               transparent={true}
               visible={this.state.dateModalVisible}
             >
-              <View
-                style={{
-                  backgroundColor: '#fff',
-                  top: 10,
-                  padding: 20,
-                  shadowColor: '#000',
-                  shadowOffset: { width: 0, height: 2 },
-                  shadowOpacity: 0.23,
-                  shadowRadius: 2.62,
-                  elevation: 4
-                }}
-              >
+              <View style={sharedStyles.datePickerModal}>
                 <DatePickerIOS
                   mode="date"
                   date={this.state.date}
@@ -134,84 +107,136 @@ class TransactionForm extends React.Component {
           </View>
           <View style={{ flex: 10, flexDirection: 'row' }}>
             <TextInput
+              style={sharedStyles.amountInput}
               value={this.state.amount}
               keyboardType="number-pad"
               onChangeText={this.onChangeText}
-              style={{
-                flex: 1,
-                fontSize: largeFontSize,
-                textAlign: 'right',
-                fontWeight: '500'
+              onFocus={() => {
+                if (!this.state.isExpanded) {
+                  LayoutAnimation.easeInEaseOut();
+                  this.setState({ isExpanded: true });
+                }
               }}
             />
           </View>
-        </View>
-        <TextInput
-          value={this.state.memo}
-          placeholder="memo"
-          style={{
-            flex: 1,
-            textAlign: 'right',
-            fontSize: smallFontSize,
-            fontWeight: '500',
-            padding: 12,
-            borderBottomColor: inputBorderColor,
-            borderBottomWidth: 1,
-            backgroundColor: '#fff',
-            color: theme.colors.darkerGray
-          }}
-          onChangeText={text => this.setState({ memo: text })}
-        />
-        <View
-          style={{
-            backgroundColor: '#fff',
-            padding: 12,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center'
-          }}
-        >
-          <Text style={{ color: theme.colors.darkGray }}>Income</Text>
-          <Switch
-            value={this.state.isCredit}
-            onValueChange={val => this.setState({ isCredit: val })}
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            padding: 12,
-            justifyContent: 'space-between'
-          }}
-        >
-          <Button
-            label="Cancel"
-            onPress={() => this.setState({ ...defaultState })}
-            style={[sharedStyles.button]}
-            color={theme.colors.darkGray}
-          />
-          <Button
-            color={theme.colors.primary}
-            disabled={this.state.amount === '$ 00.00' || this.state.memo === ''}
-            label="Add"
-            onPress={() => {
-              addTransaction({
-                date: this.state.date,
-                memo: this.state.memo,
-                amount: this.state.amount,
-                isCredit: this.state.isCredit
-              });
-              this.setState({ ...defaultState });
-            }}
-            style={[sharedStyles.button]}
-          />
-        </View>
+        </TouchableOpacity>
+        {this.state.isExpanded && [
+          <TextInput
+            key="memoInput"
+            style={sharedStyles.memoInput}
+            value={this.state.memo}
+            placeholder="memo"
+            onChangeText={text => this.setState({ memo: text })}
+          />,
+          <View key="creditSwitch" style={sharedStyles.isCreditSwitch}>
+            <Text style={{ color: theme.colors.darkGray }}>Income</Text>
+            <Switch
+              value={this.state.isCredit}
+              onValueChange={val => this.setState({ isCredit: val })}
+            />
+          </View>,
+          <View key="buttons" style={sharedStyles.buttonContainer}>
+            <Button
+              label="Cancel"
+              onPress={() => {
+                LayoutAnimation.easeInEaseOut();
+                this.setState(getDefaultState());
+              }}
+              style={[sharedStyles.button]}
+              color={theme.colors.darkGray}
+            />
+            <Button
+              color={theme.colors.primary}
+              disabled={
+                this.state.amount === '$ 00.00' || this.state.memo === ''
+              }
+              label="Add"
+              onPress={() => {
+                addTransaction({
+                  date: this.state.date,
+                  memo: this.state.memo,
+                  amount: this.state.amount,
+                  isCredit: this.state.isCredit
+                });
+                LayoutAnimation.easeInEaseOut();
+                this.setState(getDefaultState());
+              }}
+              style={[sharedStyles.button]}
+            />
+          </View>
+        ]}
       </View>
     );
   }
 }
 
 const sharedStyles = StyleSheet.create({
+  container: {
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 5,
+    backgroundColor: theme.colors.backgroundColor,
+    borderRadius: 10
+  },
+  firstRow: {
+    flexDirection: 'row',
+    borderBottomColor: inputBorderColor,
+    borderBottomWidth: 1,
+    padding: 12,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10
+  },
+  dateText: {
+    flex: 9,
+    fontSize: smallFontSize,
+    color: theme.colors.darkGray,
+    fontWeight: '500'
+  },
+  datePickerModal: {
+    backgroundColor: '#fff',
+    top: 10,
+    padding: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4
+  },
+  amountInput: {
+    flex: 1,
+    fontSize: largeFontSize,
+    textAlign: 'right',
+    fontWeight: '500'
+  },
+  memoInput: {
+    flex: 1,
+    textAlign: 'right',
+    fontSize: smallFontSize,
+    fontWeight: '500',
+    padding: 12,
+    borderBottomColor: inputBorderColor,
+    borderBottomWidth: 1,
+    backgroundColor: '#fff',
+    color: theme.colors.darkerGray
+  },
+  isCreditSwitch: {
+    backgroundColor: '#fff',
+    padding: 12,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    padding: 12,
+    justifyContent: 'space-between'
+  },
   button: {
     paddingLeft: 20,
     paddingRight: 20,
