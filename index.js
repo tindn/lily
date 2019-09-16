07b2ed85a3d2ed7edcd71adfa1aecd5a7f5d048a
Tin Nguyen <tindn14@gmail.com>
2019-09-16 18:07:04 -0400

    rename app folder to js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-/** @format */
-import { YellowBox } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
+import App from './js/app';
+
 if (__DEV__) {
   YellowBox.ignoreWarnings(['Remote debugger']);
 }
-import { AppRegistry } from 'react-native';
-import App from './app';
 
 AppRegistry.registerComponent('lily', () => App);
